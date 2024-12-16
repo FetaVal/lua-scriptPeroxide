386c314ba2554f8c957e35831205789e1fe9cdc1
Feta Val <fetavalentinejzy@gmail.com>
2024-12-16 17:21:41 +0300

    Peroxide script.lua

---
+++ script.lua
@@ -0,0 +1,53 @@
+-- Auto Farm
+game.Players.LocalPlayer.PlayerScripts.PeroxideScript.AutoFarm = true
+
+-- Auto Quests
+game.Players.LocalPlayer.PlayerScripts.PeroxideScript.AutoQuests = true
+
+-- Auto Farm Hollow Quest
+game.Players.LocalPlayer.PlayerScripts.PeroxideScript.AutoFarmHollowQuest = true
+
+-- Auto Farm Shikai
+game.Players.LocalPlayer.PlayerScripts.PeroxideScript.AutoFarmShikai = true
+
+-- Kill Aura
+game.Players.LocalPlayer.PlayerScripts.PeroxideScript.KillAura = true
+
+-- Anticheat
+game.Players.LocalPlayer.PlayerScripts.PeroxideScript.Anticheat = false
+
+-- Infinite Spins
+game.Players.LocalPlayer.PlayerScripts.PeroxideScript.InfiniteSpins = true
+
+-- Functions
+while true do
+if game.Players.LocalPlayer.PlayerScripts.PeroxideScript.AutoFarm == true then
+-- Auto Farm Code Here
+end
+
+if game.Players.LocalPlayer.PlayerScripts.PeroxideScript.AutoQuests == true then
+-- Auto Quests Code Here
+end
+
+if game.Players.LocalPlayer.PlayerScripts.PeroxideScript.AutoFarmHollowQuest == true then
+-- Auto Farm Hollow Quest Code Here
+end
+
+if game.Players.LocalPlayer.PlayerScripts.PeroxideScript.AutoFarmShikai == true then
+-- Auto Farm Shikai Code Here
+end
+
+if game.Players.LocalPlayer.PlayerScripts.PeroxideScript.KillAura == true then
+-- Kill Aura Code Here
+end
+
+if game.Players.LocalPlayer.PlayerScripts.PeroxideScript.Anticheat == true then
+-- Anticheat Code Here
+end
+
+if game.Players.LocalPlayer.PlayerScripts.PeroxideScript.InfiniteSpins == true then
+-- Infinite Spins Code Here
+end
+
+wait(0.1)
+end
